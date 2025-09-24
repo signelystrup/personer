5e50e028c9f0e7633665e8aa66d70cfd03de602e
signelystrup <166133407+signelystrup@users.noreply.github.com>
2025-09-24 11:06:02 +0200

    bleh

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+#include "personer.h"
+
 int main(void) {
     printf("Hello, World!\n");
+
+    struct person_t *person = make_person("Hanne", "Nielsen");
+
+    printf("%s", person->first_name );
+
     return 0;
 }
--- personer.c
+++ personer.c
@@ -1,11 +1,26 @@
 #include "personer.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 struct person_t *make_person(char *first_name, char *last_name) {
     // TODO: Allokér hukommelse på heap'en til min struct person_t
     // TODO: Lav en kopi af first_name og last_name og brug kopierne
 
-    struct person_t *new;
-    new->first_name = first_name;
-    new->last_name = last_name;
+
+    char* first_name_2 = malloc( strlen(first_name) + 1);
+    char* last_name_2  = malloc( strlen(last_name) + 1);
+
+    strcpy(first_name_2, first_name);
+    strcpy(last_name_2 , last_name);
+
+    struct person_t *new = malloc(sizeof(struct person_t) );;
+    new->first_name = first_name_2;
+    new->last_name = last_name_2;
+
+    //free(first_name_2);
+    //free(last_name_2);
+
     return new;
 }
